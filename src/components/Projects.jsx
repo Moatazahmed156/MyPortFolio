29de7ee7dffee3bde5ca86d7a4bf921a9cf7b915
Moataz Ahmed <146819341+Moatazahmed156@users.noreply.github.com>
2025-07-21 15:31:44 +0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,7 +30,7 @@ export default function ProjectsScreen() {
                   />
                 ))}
               </div>
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 fade-in-bottom">
                 {Projects[selectedIndex].list.map((project, index) => (
                   <ProjectCard
                     key={index}
